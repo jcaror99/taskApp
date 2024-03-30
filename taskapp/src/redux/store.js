d8136a757af1be055643c46ed3b13338633198e2
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2024-03-30 14:44:52 -0500

    finalCommit

--- taskapp/src/redux/store.js
+++ taskapp/src/redux/store.js
@@ -1,6 +1,5 @@
 import { createStore } from "redux";
 import { devToolsEnhancer } from "@redux-devtools/extension";
-import { statusFilters } from "./constants";
 
 const initialState = {
   tasks: [
